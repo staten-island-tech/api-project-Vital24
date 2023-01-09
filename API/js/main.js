f61573bdbdf953049d4d13299502a419d5120361
Vital24 <zynn00001@gmail.com>
2023-01-09 10:16:28 -0500

    BRO

--- API/js/main.js
+++ API/js/main.js
@@ -1,7 +1,9 @@
-import "../style/style.css";
+// import "../style/style.css";
 const URL = `https://api.jikan.moe/v4/anime?q=Naruto&sfw`;
 // const URL = `https://api.jikan.moe/v4/naruto/{id}/picturens`;
 // const URL = `https://api.jikan.moe/v4/anime?q=${anime}&sfw`;
+
+console.log(URL);
 
 async function getData(URL) {
   try {
@@ -10,7 +12,13 @@ async function getData(URL) {
       throw new Error(aniout);
     } else {
       const data = await aniout.json();
-      document.body.textContent = data.content;
+      console.log(data.data);
+      console.log(data.data[0].images.jpg.large_image_url);
+      data.forEach((el) => cel.data  { document.body.innerHTML = `<img src="${el.images.jpg.large_image_url}" alt="">`;
+      console.log("poop");}
+
+        );
+      document.body.innerHTML = `<img src="${data.data[0].images.jpg.large_image_url}" alt="">`;
       console.log("poop");
     }
   } catch (error) {
