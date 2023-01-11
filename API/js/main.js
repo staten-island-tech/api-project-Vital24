ea03005844924a986a1d516c67b4d29d09b681a9
Vital24 <zynn00001@gmail.com>
2023-01-11 10:21:59 -0500

    IDEA

--- API/js/main.js
+++ API/js/main.js
@@ -1,5 +1,5 @@
-// import "../style/style.css";
-const URL = `https://api.jikan.moe/v4/anime?q=Naruto&sfw`;
+import "../style/style.css";
+const URL = `https://api.jikan.moe/v4/anime/{id}/episodes`;
 // const URL = `https://api.jikan.moe/v4/naruto/{id}/picturens`;
 // const URL = `https://api.jikan.moe/v4/anime?q=${anime}&sfw`;
 
@@ -15,14 +15,14 @@ async function getData(URL) {
       // console.log(data.data);
       // console.log(data.data[0].images.jpg.large_image_url);
       console.log(data.data);
-      GetImg();
+      GetImg(URL);
       // data.data.forEach((el) => {
       //   document.body.insertAdjacentHTML(
       //     "afterbegin",
       //     `<img src="${el.images.jpg.large_image_url}" alt="">`
       //   );
-        console.log("poop");
-      };
+      //   console.log("poop");
+      // });
       // document.body.innerHTML = `<img src="${data.data[0].images.jpg.large_image_url}" alt="">`;
       // console.log("poop");
     }
@@ -32,7 +32,9 @@ async function getData(URL) {
   }
 }
 
-function GetImg() {
+async function GetImg(URL) {
+  const aniout = await fetch(URL);
+  const data = await aniout.json();
   data.data.forEach((el) => {
     document.body.insertAdjacentHTML(
       "afterbegin",
@@ -42,4 +44,55 @@ function GetImg() {
   });
 }
 
+// GetImg();
 getData(URL);
+
+const products = {
+  getImg: function () {
+    data
+      .filter((el) => el.type.includes(`meat`))
+      .forEach((el) => {
+        DOM.box.insertAdjacentHTML(
+          "afterbegin",
+          `<div class= "PCards">
+          <img src="${el.img}"/>
+            <h1>${el.name}</h1>
+          </div>
+        `
+        );
+      });
+  },
+  getName: function () {
+    data
+      .filter((el) => el.type.includes(`sushi`))
+      .forEach((el) => {
+        DOM.box.insertAdjacentHTML(
+          "afterbegin",
+          `<div class= "PCards">
+          <img src="${el.img}"/>
+            <h1>${el.name}</h1>
+          </div>
+        `
+        );
+      });
+  },
+  getDrinks: function () {
+    data
+      .filter((el) => el.type.includes(`drinks`))
+      .map((el) => {
+        DOM.box.insertAdjacentHTML(
+          "afterbegin",
+          `<div class= "PCards">
+          <img src="${el.img}"/>
+            <h1>${el.name}</h1>
+          </div>
+        `
+        );
+      });
+  },
+  getAll: function () {
+    products.getMeat();
+    products.getSushi();
+    products.getDrinks();
+  },
+};
