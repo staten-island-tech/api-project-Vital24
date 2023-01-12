d3294cb6005a1959d66d8f3f0c7668d3ca5a8707
Vital24 <113121145+Vital24@users.noreply.github.com>
2023-01-11 21:53:22 -0500

    Progress baby

--- API/js/main.js
+++ API/js/main.js
@@ -1,4 +1,5 @@
-// import "../style/style.css";
+import { DOM } from "./dom";
+import "../style/style.css";
 const URL = `https://api.jikan.moe/v4/anime?q=Naruto&sfw`;
 // const URL = `https://api.jikan.moe/v4/naruto/{id}/picturens`;
 // const URL = `https://api.jikan.moe/v4/anime?q=${anime}&sfw`;
@@ -15,31 +16,40 @@ async function getData(URL) {
       // console.log(data.data);
       // console.log(data.data[0].images.jpg.large_image_url);
       console.log(data.data);
-      GetImg();
+      info.GetImg();
       // data.data.forEach((el) => {
       //   document.body.insertAdjacentHTML(
       //     "afterbegin",
       //     `<img src="${el.images.jpg.large_image_url}" alt="">`
       //   );
-        console.log("poop");
-      };
-      // document.body.innerHTML = `<img src="${data.data[0].images.jpg.large_image_url}" alt="">`;
-      // console.log("poop");
+      console.log("poop");
     }
+    // document.body.innerHTML = `<img src="${data.data[0].images.jpg.large_image_url}" alt="">`;
+    // console.log("poop");
   } catch (error) {
     console.log(error);
     console.log("No");
   }
 }
 
-function GetImg() {
-  data.data.forEach((el) => {
-    document.body.insertAdjacentHTML(
-      "afterbegin",
-      `
-      <img src="${el.images.jpg.large_image_url}" alt="">`
-    );
-  });
-}
+const info = {
+  GetImg: async function () {
+    const aniout = await fetch(URL);
+    const data = await aniout.json();
+    data.data.forEach((el) => {
+      DOM.reponse.insertAdjacentHTML(
+        "afterbegin",
+        `
+        <img src="${el.images.jpg.large_image_url}" alt="">
+        <h2>Name:${el.title_english} ID: ${el.mal_id}</h2>`
+      );
+    });
+  },
+};
 
 getData(URL);
+
+DOM.Submit.addEventListener("submit", function (abc) {
+  abc.preventDefault();
+  document.getElementById("form").reset();
+});
