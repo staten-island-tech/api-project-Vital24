58a308dcdacb1c322c1391332b9b59efb88c631e
Vital24 <zynn00001@gmail.com>
2023-01-18 10:11:27 -0500

    aaaa

--- API/js/main.js
+++ API/js/main.js
@@ -1,9 +1,6 @@
 import { DOM } from "./dom";
 import "../style/style.css";
-const URL = `https://api.jikan.moe/v4/anime?q=one-piece&sfw`;
-
-// const URL = `https://api.jikan.moe/v4/naruto/{id}/picturens`;
-// const URL = `https://api.jikan.moe/v4/anime?q=${anime}&sfw`;
+const URL = `https://api.jikan.moe/v4/anime?q=mob pyscho&sfw`;
 
 async function getData(URL) {
   try {
@@ -11,24 +8,14 @@ async function getData(URL) {
     if (aniout.status <= 199 || aniout.status >= 300) {
       throw new Error(aniout);
     } else {
-      const data = await aniout.json();
-
       info.GetImg();
-      // data.data.forEach((el) => {
-      //   document.body.insertAdjacentHTML(
-      //     "afterbegin",
-      //     `<img src="${el.images.jpg.large_image_url}" alt="">`
-      //   );
     }
-    // document.body.innerHTML = `<img src="${data.data[0].images.jpg.large_image_url}" alt="">`;
-    // console.log("poop");
   } catch (error) {
     console.log(error);
     console.log("No");
   }
 }
 
-// const EPID = data.data.forEach((el) => el.mal_id);
 const info = {
   GetImg: async function () {
     const aniout = await fetch(URL);
@@ -41,6 +28,7 @@ const info = {
         `<div class=>
         <img src="${el.images.jpg.large_image_url}" alt="">
         <h2>Name:${el.title_english} ID: ${el.mal_id}</h2>
+        <button class = "ShowEP id = "${el.mal_id}">Show Episodes</button>
         </div>
         `
       );
@@ -56,18 +44,15 @@ const info = {
     });
     console.log(URLEParr);
 
-    for (const a in URLEParr) {
-      setInterval(async () => {
-        const epout = await fetch(`${URLEParr[a]}`);
-        const epdata = await epout.json();
+    // for (const a in URLEParr) {
+    //   setInterval(async function episodes() {
+    //     const epout = await fetch(`${URLEParr[a]}`);
+    //     const epdata = await epout.json();
+    //     console.log(epdata);
+    //   }, 1000);
 
-        console.log(epdata);
-      }, 400);
-
-      /* const epout = await fetch(`${URLEParr[a]}`);
-      const epdata = await epout.json();
-      console.log(epdata); */
-    }
+    // console.log(epdata);
+    // }
   },
 
   GetEp: async function () {
@@ -87,7 +72,6 @@ const info = {
     });
   },
 };
-
 // info.GetEp();
 // GetImg();
 getData(URL);
