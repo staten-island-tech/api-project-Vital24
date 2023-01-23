a147f06b5e443e2d67c26ebd9240791ae3ba5f64
Vital24 <zynn00001@gmail.com>
2023-01-23 10:16:42 -0500

    Some

--- API/js/main.js
+++ API/js/main.js
@@ -21,45 +21,50 @@ const info = {
   GetImg: async function () {
     const GunInfo = await fetch(URL);
     const GunData = await GunInfo.json();
-    console.log(GunData);
-    AA = GunData.data.filter((el) => el.displayName.includes("Odin"));
-    console.log(AA);
-
-    // data.data.forEach((el) => {
-    // DOM.reponse.insertAdjacentHTML(
-    //   "beforeend",
-    //   `<div class=>
-    //   <img src="${el.images.jpg.large_image_url}" alt="">
-    //   <h2>Name:${el.title_english} ID: ${el.mal_id}</h2>
-    //   <button class = "ShowEP id = "${el.mal_id}">Show Episodes</button>
-    //   </div>
-    //   `
-    //   );
-    // });
-  },
-
-  GetEp: async function () {
-    const URLEP = `https://api.jikan.moe/v4/anime/20/episodes`;
-    const epout = await fetch(URLEP);
-    const epdata = await epout.json();
-
-    console.log(epdata.data.title_english);
-    epdata.data.forEach((el) => {
-      DOM.reponse.insertAdjacentHTML(
-        "beforeend",
-        `<div class=>>
-        <h2>Name:${el.title}</h2>
-        </div>
-        `
-      );
-    });
+    console.log(GunData.data);
+    GunData.data
+      .filter((el) => el.displayName.includes("Odin"))
+      .map((el) => {
+        console.log(el.displayName);
+      });
   },
 };
+info.GetImg();
+// data.data.forEach((el) => {
+// DOM.reponse.insertAdjacentHTML(
+//   "beforeend",
+//   `<div class=>
+//   <img src="${el.images.jpg.large_image_url}" alt="">
+//   <h2>Name:${el.title_english} ID: ${el.mal_id}</h2>
+//   <button class = "ShowEP id = "${el.mal_id}">Show Episodes</button>
+//   </div>
+//   `
+//   );
+// });
+// },
+
+//   GetEp: async function () {
+//     const URLEP = `https://api.jikan.moe/v4/anime/20/episodes`;
+//     const epout = await fetch(URLEP);
+//     const epdata = await epout.json();
+
+//     console.log(epdata.data.title_english);
+//     epdata.data.forEach((el) => {
+//       DOM.reponse.insertAdjacentHTML(
+//         "beforeend",
+//         `<div class=>>
+//         <h2>Name:${el.title}</h2>
+//         </div>
+//         `
+//       );
+//     });
+//   },
+// };
 // info.GetEp();
 // GetImg();
-getData(URL);
+// getData(URL);
 
-DOM.Submit.addEventListener("submit", function (abc) {
-  abc.preventDefault();
-  document.getElementById("form").reset();
-});
+// DOM.Submit.addEventListener("submit", function (abc) {
+//   abc.preventDefault();
+//   document.getElementById("form").reset();
+// });
