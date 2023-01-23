1233f9ad53272c8467ba19f2fb964e1da9baefdb
Vital24 <zynn00001@gmail.com>
2023-01-23 09:43:27 -0500

    ????

--- API/js/main.js
+++ API/js/main.js
@@ -1,12 +1,13 @@
 import { DOM } from "./dom";
 import "../style/style.css";
-const URL = ``;
+const URL = `https://valorant-api.com/v1/weapons`;
 
 async function getData(URL) {
   try {
-    const aniout = await fetch(URL);
-    if (aniout.status <= 199 || aniout.status >= 300) {
-      throw new Error(aniout);
+    const GunInfo = await fetch(URL);
+
+    if (GunInfo.status <= 199 || GunInfo.status >= 300) {
+      throw new Error(GunInfo);
     } else {
       info.GetImg();
     }
@@ -18,41 +19,23 @@ async function getData(URL) {
 
 const info = {
   GetImg: async function () {
-    const aniout = await fetch(URL);
-    const data = await aniout.json();
-    const EPID = [];
+    const GunInfo = await fetch(URL);
+    const GunData = await GunInfo.json();
+    console.log(GunData);
+    AA = GunData.data.filter((el) => el.displayName.includes("Odin"));
+    console.log(AA);
 
-    data.data.forEach((el) => {
-      DOM.reponse.insertAdjacentHTML(
-        "beforeend",
-        `<div class=>
-        <img src="${el.images.jpg.large_image_url}" alt="">
-        <h2>Name:${el.title_english} ID: ${el.mal_id}</h2>
-        <button class = "ShowEP id = "${el.mal_id}">Show Episodes</button>
-        </div>
-        `
-      );
-      EPID.push(`${el.mal_id}`);
-    });
-    //IM a GENIUS
-    const URLEParr = [];
-
-    EPID.forEach((el) => {
-      `https://api.jikan.moe/v4/anime/${el}/episodes`;
-
-      URLEParr.push(`https://api.jikan.moe/v4/anime/${el}/episodes`);
-    });
-    console.log(URLEParr);
-
-    // for (const a in URLEParr) {
-    //   setInterval(async function episodes() {
-    //     const epout = await fetch(`${URLEParr[a]}`);
-    //     const epdata = await epout.json();
-    //     console.log(epdata);
-    //   }, 1000);
-
-    // console.log(epdata);
-    // }
+    // data.data.forEach((el) => {
+    // DOM.reponse.insertAdjacentHTML(
+    //   "beforeend",
+    //   `<div class=>
+    //   <img src="${el.images.jpg.large_image_url}" alt="">
+    //   <h2>Name:${el.title_english} ID: ${el.mal_id}</h2>
+    //   <button class = "ShowEP id = "${el.mal_id}">Show Episodes</button>
+    //   </div>
+    //   `
+    //   );
+    // });
   },
 
   GetEp: async function () {
